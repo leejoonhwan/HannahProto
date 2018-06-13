c76c271e90c313909016e6c749ba40238c183180
dyhan <dyhan@sk.com>
2018-06-13 17:36:40 +0900

    code cleansing

--- Vue_Full_Project/src/views/common/BarTopHalf.vue
+++ Vue_Full_Project/src/views/common/BarTopHalf.vue
@@ -66,28 +66,6 @@ export default {
       return result
     }
   },
-  computed: {
-    // chartData () {
-    // return {
-    //   labels: ['5/15', '5/16', '5/17', '5/18', '5/19', '5/20', '5/21', '5/22', '5/23', '5/24', '5/25', '', '', ''],
-    //   datasets: [{
-    //     label: 'Data11',
-    //     backgroundColor: ['rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)'],
-    //     borderColor: 'transparent',
-    //     yAxisID: 'left-y-axis',
-    //     data: [30, 22, 24, 35, 30, 34, 56, 74, 22, 17, 32, 58, 54, 62]
-    //   },
-    //   {
-    //     label: 'Data12',
-    //     backgroundColor: 'transparent', // 'rgba(255,255,255,.3)',
-    //     borderColor: 'rgba(255,255,255,.5)',
-    //     yAxisID: 'right-y-axis',
-    //     type: 'line',
-    //     data: [1130, 1122, 1214, 1315, 1350, 1374, 1416, 1514, 1622, 1617, 1732, 1758, 1854, 1862]
-    //   }]
-    // }
-    // }
-  },
   created () {
     fetch(this.config[this.dataType].api)
       .then(res => res.json())
