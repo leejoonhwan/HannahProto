1a03b220d81cd421e08ef83397cff99a72297a42
dyhan <dyhan@sk.com>
2018-06-13 17:36:09 +0900

    chart working with apis

--- Vue_Full_Project/src/views/common/BarTopHalf.vue
+++ Vue_Full_Project/src/views/common/BarTopHalf.vue
@@ -20,6 +20,7 @@ export default {
   props: ['data-type', 'bg'],
   data () {
     return {
+      fetchedData: {},
       config: {
         'member-count': {
           title: '멤버십 회원 / Members',
@@ -31,30 +32,69 @@ export default {
           desc: '당일 345건 (+34 명)',
           api: '/static/dummy/getMemberVisitCount'
         }
+      },
+      chartData: {}
+    }
+  },
+  methods: {
+    makeChartData (rawJson) {
+      let result = {
+        labels: [],
+        datasets: [
+          {
+            label: '',
+            backgroundColor: [],
+            borderColor: 'transparent',
+            yAxisID: 'left-y-axis',
+            data: []
+          }, {
+            label: '',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.5)',
+            yAxisID: 'right-y-axis',
+            type: 'line',
+            data: []
+          }]
       }
+      for (let d of rawJson.data) {
+        result.labels.push(d.date)
+        result.datasets[0].data.push(d.new)
+        result.datasets[1].data.push(d.accum)
+        result.datasets[0].backgroundColor.push(d.wday.startsWith('S') ? 'rgba(255,55,55,.3)' : 'rgba(255,255,255,.3)')
+      }
+      console.log(result)
+      return result
     }
   },
   computed: {
-    chartData () {
-      return {
-        labels: ['5/15', '5/16', '5/17', '5/18', '5/19', '5/20', '5/21', '5/22', '5/23', '5/24', '5/25', '', '', ''],
-        datasets: [{
-          label: 'Data11',
-          backgroundColor: ['rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)'],
-          borderColor: 'transparent',
-          yAxisID: 'left-y-axis',
-          data: [30, 22, 24, 35, 30, 34, 56, 74, 22, 17, 32, 58, 54, 62]
-        },
-        {
-          label: 'Data12',
-          backgroundColor: 'transparent', // 'rgba(255,255,255,.3)',
-          borderColor: 'rgba(255,255,255,.5)',
-          yAxisID: 'right-y-axis',
-          type: 'line',
-          data: [1130, 1122, 1214, 1315, 1350, 1374, 1416, 1514, 1622, 1617, 1732, 1758, 1854, 1862]
-        }]
-      }
-    }
+    // chartData () {
+    // return {
+    //   labels: ['5/15', '5/16', '5/17', '5/18', '5/19', '5/20', '5/21', '5/22', '5/23', '5/24', '5/25', '', '', ''],
+    //   datasets: [{
+    //     label: 'Data11',
+    //     backgroundColor: ['rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,55,55,.3)', 'rgba(255,55,55,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)', 'rgba(255,255,255,.3)'],
+    //     borderColor: 'transparent',
+    //     yAxisID: 'left-y-axis',
+    //     data: [30, 22, 24, 35, 30, 34, 56, 74, 22, 17, 32, 58, 54, 62]
+    //   },
+    //   {
+    //     label: 'Data12',
+    //     backgroundColor: 'transparent', // 'rgba(255,255,255,.3)',
+    //     borderColor: 'rgba(255,255,255,.5)',
+    //     yAxisID: 'right-y-axis',
+    //     type: 'line',
+    //     data: [1130, 1122, 1214, 1315, 1350, 1374, 1416, 1514, 1622, 1617, 1732, 1758, 1854, 1862]
+    //   }]
+    // }
+    // }
+  },
+  created () {
+    fetch(this.config[this.dataType].api)
+      .then(res => res.json())
+      .then(response => {
+        // this.fetchedData = response
+        this.chartData = this.makeChartData(response)
+      })
   }
 }
 </script>
