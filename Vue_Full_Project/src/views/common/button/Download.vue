af6316088a411dcfbea5e478d2e3e49dc75c281a
dyhan <dyhan@sk.com>
2018-06-26 14:28:49 +0900

    create download button component

---
+++ Vue_Full_Project/src/views/common/button/Download.vue
@@ -0,0 +1,23 @@
+<template>
+  <i class="icon-cloud-download icons p-2" style="color:green;" v-on:click = "downloadExcel()"></i>
+</template>
+
+<script>
+export default {
+  name: 'ExcelDownLoadComponent',
+  props: ['downloadUrl'],
+  methods: {
+    downloadExcel () {
+      console.log(this.downloadUrl)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .icons {
+    font-size: 18px;
+    padding-right: 7px;
+    font-weight: bold;
+  }
+</style>
