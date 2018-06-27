305f8652a0f73dddd715283fdd3ccd7c057a45f1
dyhan <dyhan@sk.com>
2018-06-27 18:57:48 +0900

    find better options of Age and Pie charts

--- Vue_Full_Project/src/views/common/charts/AgeChart.vue
+++ Vue_Full_Project/src/views/common/charts/AgeChart.vue
@@ -1,25 +1,59 @@
 <script>
-import {Bar} from 'vue-chartjs'
+import {HorizontalBar} from 'vue-chartjs'
 
 export default {
   name: 'AgeChart',
-  extends: Bar,
+  extends: HorizontalBar,
   data () {
     return {
       datacollection: {
-        labels: ['January', 'February'],
+        labels: ['60+', '50대', '40대', '30대', '20대', '10대'],
         datasets: [
           {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20]
+            label: '남성',
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 1,
+            data: [-1.2, -9.2, -18.1, -19.3, -16.1, -5.3]
+          },
+          {
+            label: '여성',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 1,
+            data: [5.2, 12.4, 20.1, 25.3, 22.1, 6.3]
           }
         ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        // type: 'horizontalBar',
+        scales: {
+          xAxes: [{
+            stacked: true,
+            ticks: {
+              callback: function (value, index, values) {
+                return Math.abs(value) + '%'
+              }
+            }
+          }],
+          yAxes: [{
+            stacked: true
+          }]
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem, data) {
+              return Math.abs(tooltipItem.xLabel) + '%'
+            }
+          }
+        }
       }
     }
   },
   mounted () {
-    this.renderChart(this.datacollection, {responsive: true, maintainAspectRatio: false})
+    this.renderChart(this.datacollection, this.options)
   }
 }
 </script>
--- Vue_Full_Project/src/views/common/charts/PieChart.vue
+++ Vue_Full_Project/src/views/common/charts/PieChart.vue
@@ -7,19 +7,41 @@ export default {
   data () {
     return {
       datacollection: {
-        labels: ['January', 'February'],
+        labels: ['Family', 'Welcome', 'VIP', 'Friends'],
         datasets: [
           {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20]
+            label: 'Grade',
+            // red: 'rgb(255, 99, 132)',
+            // orange: 'rgb(255, 159, 64)',
+            // yellow: 'rgb(255, 205, 86)',
+            // green: 'rgb(75, 192, 192)',
+            // blue: 'rgb(54, 162, 235)',
+            // purple: 'rgb(153, 102, 255)',
+            // grey: 'rgb(201, 203, 207)'
+            backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(255, 159, 64, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)'],
+            borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)'],
+            borderWidth: 1,
+            data: [40, 20, 10, 30]
           }
         ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: { display: false },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem, data) {
+              console.log(tooltipItem)
+              return Math.abs(tooltipItem.xLabel) + '%'
+            }
+          }
+        }
       }
     }
   },
   mounted () {
-    this.renderChart(this.datacollection, {responsive: true, maintainAspectRatio: false})
+    this.renderChart(this.datacollection, this.options)
   }
 }
 </script>
